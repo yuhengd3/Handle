ff3a3d925a0832c2c58b23880b6e3506e08b853e
Yuheng Ding <d.yuheng@wustl.edu>
2022-03-16 10:08:22 -0500

    Initial Commit

---
+++ Handle/HandleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandleApp.swift
+//  Handle
+//
+//  Created by Yuheng Ding on 2022/3/16.
+//
+
+import SwiftUI
+
+@main
+struct HandleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
